d4430d0240f4f07fde2dc3a8ef6c0cc22f0b27ad
Tatsuya Sato <satoryu.1981@gmail.com>
2021-09-27 10:21:51 +0900

    検索機能の実装

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,13 @@
 class BooksController < ApplicationController
   before_action :load_genres
+  before_action :load_genre, only: %i[search]
+  before_action :load_book, only: %i[show]
+
+  rescue_from StandardError do |exception|
+    Rails.logger.error exception.message
+
+    redirect_to books_path
+  end
 
   def index
   end
@@ -7,8 +15,21 @@ class BooksController < ApplicationController
   def show
   end
 
+  def search
+    @books = @genre.books
+    @books = @books.where(title: params[:title]) if params[:title]
+  end
+
   private
   def load_genres
     @genres = Genre.all
+  end
+
+  def load_genre
+    @genre = Genre.find(params[:genre_id])
+  end
+
+  def load_book
+    @book = Book.find(params[:id])
   end
 end
