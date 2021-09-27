b955f61d1e03204bd77a98bac41ed736c4ddd375
Tatsuya Sato <satoryu.1981@gmail.com>
2021-09-27 11:18:14 +0900

    ジャンル選択時にタイトルの選択肢を更新する

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,7 +7,7 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
-
+require('jquery')
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
@@ -15,3 +15,36 @@ require("channels")
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+
+$(document).on('turbolinks:load', function () {
+  let $search_form = $('#search_form')
+  let $genre_select = $('select#genre_id', $search_form)
+  let $title_select = $('select#title', $search_form)
+
+  function updateTitleSelect(genre_id) {
+    $.ajax({
+      url: '/api/books',
+      data: { genre_id },
+      success: function (data) {
+        console.log(data)
+        $('option', $title_select).remove() // 最初にoptionを空にする
+
+        // APIからのレスポンス（本の情報の配列）を処理する
+        data.forEach((book) => {
+          let option = $('<option/>') // 新しくoptionタグを生成
+          option.attr({ value: book.title })
+          option.text(book.title) // 必要な情報を更新
+          $title_select.append(option)
+        })
+      }
+    })
+  }
+
+  $genre_select.on('change', function() {
+    const option = $('option:selected', this)
+
+    if (option) {
+      updateTitleSelect(option.val())
+    }
+  })
+})
